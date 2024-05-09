a165eeac75fd4ae4bdc900f1987f07cb3657a60c
Navarro2308 <142362694+Navarro2308@users.noreply.github.com>
2024-05-09 12:13:23 -0300

    Update teste-app.sh

--- teste-app.sh
+++ teste-app.sh
@@ -1,14 +1,15 @@
-#/bin/bash 
-RESULT="`wget -qO- http://localhost:8090`"
-wget -q localhost:8090 
-if [ $? -eq 0 ] 
-then
+#!/bin/bash
+
+RESULT=$(wget -qO- http://localhost:8090)
+wget -q localhost:8090
+
+if [ $? -eq 0 ]; then
     echo 'ok - serviço no ar!'
-elif [[ $RESULT == *"Number"* ]] 
-then
+elif [[ $RESULT == *"Number"* ]]; then
     echo 'ok - número de visitas'
-    echo $RESULT
+    echo "$RESULT"
 else
     echo 'not ok - número de visitas'
     exit 1
 fi
+
